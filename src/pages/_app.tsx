e43703e8e8244a6afc4af83522dc41fe3ae2f80c
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-07 22:59:48 +0900

    style: 💄: setting next/font
    {breaking_change}

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { initializeFirebaseApp } from '~/lib/firebase'
+import { Noto_Sans_JP } from 'next/font/google'
+
+const notoSansJP = Noto_Sans_JP({
+  weight: ['300', '400', '500'],
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
 
 initializeFirebaseApp()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${notoSansJP.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main >
+  )
 }
