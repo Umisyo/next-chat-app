ec8a5cfccc4b5f9080c634cf3f4005badfb9b4c2
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-08 15:00:20 +0900

    refactor: ♻️: signupの関数を分離

--- src/component/feature/SignUp/index.tsx
+++ src/component/feature/SignUp/index.tsx
@@ -1,41 +1,23 @@
 import { ChangeEvent, FormEvent, useState } from 'react'
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  sendEmailVerification,
-} from 'firebase/auth'
-
-import { FirebaseError } from 'firebase/app'
+import { useSignUp } from '~/component/feature/SignUp/signUp.hooks'
 
 export default function SignUp() {
-  const [formData, setFormData] = useState({
+  const [formState, setFormState] = useState({
     email: '',
     password: ''
   })
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target
-    setFormData({ ...formData, [name]: value })
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormState({ ...formState, [name]: value })
   }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    try {
-      const auth = getAuth()
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        formData.email,
-        formData.password
-      )
-      await sendEmailVerification(userCredential.user)
-      setFormData({
-        email: '',
-        password: ''
-      })
-    } catch (e) {
-      if (e instanceof FirebaseError) {
-        console.log(e)
-      }
-    }
+    useSignUp(formState)
+    setFormState({
+      email: '',
+      password: ''
+    })
   }
 
   return (
@@ -43,8 +25,8 @@ export default function SignUp() {
       <div>
         <h2 className='text-2xl font-bold'>サインアップ</h2>
         <form className='mt-6' onSubmit={handleSubmit}>
-          <input className='w-96 h-8 block' type="email" name="email" value={formData.email} onChange={handleChange} required placeholder="メールアドレス" />
-          <input className='w-96 h-8 block mt-2' type="password" name="password" value={formData.password} onChange={handleChange} required placeholder="パスワード" />
+          <input className='w-96 h-8 block' type="email" name="email" value={formState.email} onChange={handleChange} required placeholder="メールアドレス" />
+          <input className='w-96 h-8 block mt-2' type="password" name="password" value={formState.password} onChange={handleChange} required placeholder="パスワード" />
           <button className='block bg-gray-700 hover:bg-gray-600 text-white px-4 py-2 mt-10' type="submit">アカウントを作成</button>
         </form>
       </div>
