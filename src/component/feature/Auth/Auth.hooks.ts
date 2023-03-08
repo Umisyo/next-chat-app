879a124f8ba3ff577fd50670d48e8be42add11ba
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-08 16:36:30 +0900

    feat: ✨: ログインページの作成

--- src/component/feature/Auth/Auth.hooks.ts
+++ src/component/feature/Auth/Auth.hooks.ts
@@ -1,5 +1,5 @@
 import { FirebaseError } from 'firebase/app'
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { toast } from 'react-toastify';
 
 export interface FormState {
@@ -7,15 +7,27 @@ export interface FormState {
   password: string;
 }
 
+const auth = getAuth()
+
 export const useSignUp = async (formState: FormState) => {
   try {
-    const auth = getAuth()
     await createUserWithEmailAndPassword(
       auth,
       formState.email,
       formState.password
     )
     toast.success('Successfully signed up!')
+  } catch (e) {
+    if (e instanceof FirebaseError) {
+      toast.error(e.message)
+    }
+  }
+}
+
+export const useLogin = async (formState: FormState) => {
+  try {
+    await signInWithEmailAndPassword(auth, formState.email, formState.password)
+    toast.success('Successfully logged in!')
   } catch (e) {
     if (e instanceof FirebaseError) {
       toast.error(e.message)
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,5 @@
+import Login from '~/component/feature/Auth/Login';
+
+export default function Page() {
+  return <Login />
+}
