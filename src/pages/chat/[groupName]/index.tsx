968340f40d4027602980defbcc51058fc8349cec
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-10 22:51:39 +0900

    fix: 🐞: chatページにAuthGuardを追加
    {breaking_change}

--- src/pages/chat/[groupName]/index.tsx
+++ src/pages/chat/[groupName]/index.tsx
@@ -1,5 +1,10 @@
+import { AuthGuard } from '~/component/feature/Auth/AuthGuard';
 import Chat from '~/component/feature/Chat/Chat';
 
 export default function Page() {
-  return <Chat />;
-};
+  return (
+    <AuthGuard>
+      <Chat />
+    </AuthGuard>
+  )
+}
