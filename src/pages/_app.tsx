73c2c42b08d938c485cff620c864c469b49cd6db
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-11 20:23:26 +0900

    fix: 🐞: titleとlangを設定
    {breaking_change}

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { AuthProvider } from '~/component/feature/Auth/AuthProvider'
 import Header from '~/component/ui/Header'
+import Head from 'next/head'
 
 const notoSansJP = Noto_Sans_JP({
   weight: ['300', '400', '500'],
@@ -17,6 +18,9 @@ initializeFirebaseApp()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Head>
+        <title>next-chat-app</title>
+      </Head>
       <Header />
       <main className={`${notoSansJP.variable} font-sans`}>
         <ToastContainer
