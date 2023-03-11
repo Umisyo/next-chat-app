a82a3619e8fafbcf2a8a02d53b322546347e6b20
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-11 20:07:02 +0900

    fix: 🐞: Headerのロゴ部分をリンク化
    {breaking_change}

--- src/component/ui/Header/index.tsx
+++ src/component/ui/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useAuthContext } from '~/component/feature/Auth/AuthProvider'
 import { useLogout } from '~/hooks/useLogout.hooks'
@@ -13,7 +14,11 @@ export default function Header() {
   return (
     <header className="w-full h-14 bg-cyan-700 p-3 sticky top-0 z-50">
       <nav className="flex w-full justify-between items-center">
-        <h1 className="text-white">NextChat</h1>
+        <h1 className="text-white">
+          <Link href="/">
+            NextChat
+          </Link>
+        </h1>
         {user ? (
           <button
             className="w-28 text-white border border-white border-dotted p-1 hover:opacity-75"
