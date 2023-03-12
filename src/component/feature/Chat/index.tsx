2ef88da3f07898d27f2328df4ec16cd06e118b49
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-12 22:15:01 +0900

    fix: 🐞: ページ遷移時に末尾までスクロールするよう変更
    {breaking_change}

--- src/component/feature/Chat/index.tsx
+++ src/component/feature/Chat/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useRef, useState } from 'react'
 import { useAuthContext } from '~/component/feature/Auth/AuthProvider'
 import { useGetChats } from '~/component/feature/Chat/hooks/useGetChats.hooks'
 import { usePostMessage } from '~/component/feature/Chat/hooks/usePostMessage.hooks'
@@ -11,6 +11,7 @@ export default function Chat() {
   const postMessage = usePostMessage
   const user = useAuthContext().user
   const [message, setMessage] = useState<string>('')
+  const scrollBottomRef = useRef<HTMLDivElement>(null);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -22,12 +23,17 @@ export default function Chat() {
 
   useRedirect(router)
 
+  useEffect(() => {
+    scrollBottomRef?.current?.scrollIntoView()
+  }, [])
+
   return (
     <div className="h-screen flex flex-col">
       <div className="pb-12 mt-auto">
         {useGetChats(router).map((chat, index) => {
           return <MessageItem key={index} message={chat} />
         })}
+        <div ref={scrollBottomRef} />
       </div>
       <form
         className="flex w-full fixed bottom-0 left-0 right-0"
