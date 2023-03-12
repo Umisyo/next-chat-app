f960990f3c1d3b6efcc443e35a229149e5768483
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-12 21:58:30 +0900

    refactor: ♻️: redirectをhooksに切り出し
    {breaking_change}

---
+++ src/component/feature/Chat/hooks/useRedirect.hooks.ts
@@ -0,0 +1,27 @@
+import { NextRouter } from 'next/router'
+import { useEffect } from 'react'
+import { get, getDatabase, ref } from 'firebase/database'
+
+const isRedirect = async (key: string) => {
+  const db = getDatabase()
+
+  const dbRef = ref(db, 'groups/' + key)
+  return await get(dbRef).then((snapshot) => {
+    return snapshot.val() === null
+  })
+}
+
+
+export default function useRedirect(router: NextRouter) {
+  const groupName = router.query.groupName
+  useEffect(() => {
+    if (router.isReady) {
+      ; (async () => {
+        if (typeof groupName !== 'string' || (await isRedirect(groupName))) {
+          router.replace('/404')
+          return
+        }
+      })()
+    }
+  }, [router])
+}
--- src/component/feature/Chat/index.tsx
+++ src/component/feature/Chat/index.tsx
@@ -1,15 +1,13 @@
 import { useRouter } from 'next/router'
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { useAuthContext } from '~/component/feature/Auth/AuthProvider'
 import { useGetChats } from '~/component/feature/Chat/hooks/useGetChats.hooks'
-import { useIsRedirect } from '~/component/feature/Chat/hooks/useIsGroupExit.hooks'
 import { usePostMessage } from '~/component/feature/Chat/hooks/usePostMessage.hooks'
+import useRedirect from '~/component/feature/Chat/hooks/useRedirect.hooks'
 import MessageItem from '~/component/feature/Chat/MessageItem'
 
 export default function Chat() {
   const router = useRouter()
-  const groupName = router.query.groupName
-  const isRedirect = useIsRedirect
   const postMessage = usePostMessage
   const user = useAuthContext().user
   const [message, setMessage] = useState<string>('')
@@ -22,20 +20,11 @@ export default function Chat() {
     }
   }
 
-  useEffect(() => {
-    if (router.isReady) {
-      ; (async () => {
-        if (typeof groupName !== 'string' || (await isRedirect(groupName))) {
-          router.replace('/404')
-          return
-        }
-      })()
-    }
-  }, [router])
+  useRedirect(router)
 
   return (
-    <div className="h-screen">
-      <div className="pb-12">
+    <div className="h-screen flex flex-col">
+      <div className="pb-12 mt-auto">
         {useGetChats(router).map((chat, index) => {
           return <MessageItem key={index} message={chat} />
         })}
